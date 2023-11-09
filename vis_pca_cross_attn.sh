44adc624e78e7b5c1b563bb03db7a7a163329e77
denghilbert <971431670@qq.com>
2023-11-09 16:47:26 -0500

    Add vis for house test case

--- vis_pca_cross_attn.sh
+++ vis_pca_cross_attn.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for i in {3..11}
+for i in {0..11}
 do
-    python run_cross_attn_pca.py --experiment cat_grass --block "output_block_$i"
+    python run_cross_attn_pca.py --experiment house_0 --block "output_block_$i"
 done
-for i in {1..11}
-do
-    python run_cross_attn_pca.py --experiment cat_grass --block "input_block_$i"
-done
+#for i in {1..11}
+#do
+#    python run_cross_attn_pca.py --experiment house_0 --block "input_block_$i"
+#done
